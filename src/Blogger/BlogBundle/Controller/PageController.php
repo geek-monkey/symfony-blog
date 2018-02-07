6954d7e3845e41502d5434938ae9eb98a84dcb8e
asma.meddeb <asma.meddeb@groupe-telnet.net>
2018-02-07 14:39:57 +0100

    - added mail sending functionality (not working)
    final commit because the tuto's symfony version is old

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -9,32 +9,40 @@ use Blogger\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+  public function indexAction()
+  {
+    return $this->render('BloggerBlogBundle:Page:index.html.twig');
+  }
+
+  public function aboutAction()
+  {
+    return $this->render('BloggerBlogBundle:Page:about.html.twig');
+  }
+
+  public function contactAction() {
+
+    $enquiry = new Enquiry();
+    $form = $this->createForm(new EnquiryType(), $enquiry);
+
+    $request = $this->getRequest();
+    if($request->getMethod() == 'POST') {
+      $form->bind($request);
+      if($form->isValid()) {
+        // send mail
+        $message = \Swift_Message::newInstance()
+        ->setSubject('Contact enquiry from symblog')
+        ->setFrom('enquiries@symblog.co.uk')
+        ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+        ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+        $this->get('mailer')->send($message);
+
+        $request->getSession()->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+        // redirect request to prevent resending the form
+        return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+      }
     }
-
-    public function aboutAction()
-   {
-       return $this->render('BloggerBlogBundle:Page:about.html.twig');
-   }
-
-   public function contactAction() {
-
-     $enquiry = new Enquiry();
-     $form = $this->createForm(new EnquiryType(), $enquiry);
-
-     $request = $this->getRequest();
-     if($request->getMethod() == 'POST') {
-       $form->handleRequest($request);
-       if($form->isValid()) {
-         // send mail
-         // redirect request to prevent resending the form
-         return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
-       }
-     }
-     return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
-       'form' => $form->createView()
-     ));
-   }
+    return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+      'form' => $form->createView()
+    ));
+  }
 }
